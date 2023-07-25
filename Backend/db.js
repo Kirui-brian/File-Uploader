7e99b7251cca6e4c7d316d3a8e1572b1850d62ca
Kirui-brian <brinkiru@gmail.com>
2023-07-25 09:51:49 +0300

    update database connection

--- Backend/db.js
+++ Backend/db.js
@@ -1,33 +1,33 @@
-// const mongoose = require('mongoose');
-
-// //MongoDB connection URL
-// const dbURL = 'mongodb://127.0.0.1:27017/file-uploader';
-
-// mongoose.connect(dbURL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// });
-
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// db.once('open', () => {
-//     console.log('Connected to MongoDB!');
-// });
-
-// module.exports = mongoose;
-
 const mongoose = require('mongoose');
-import dotenv from "dotenv"; 
 
-dotenv.config();
+//MongoDB connection URL
+const dbURL = 'mongodb://127.0.0.1:27017/file-uploader';
 
-mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => {
-        console.log("Connected to db");
-    })
-    .catch((err) => {
-        console.log(err.message);
-    });
+mongoose.connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
-    module.exports = mongoose;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB!');
+});
+
+module.exports = mongoose;
+
+// const mongoose = require('mongoose');
+// import dotenv from "dotenv"; 
+
+// dotenv.config();
+
+// mongoose
+//     .connect(process.env.MONGODB_URI)
+//     .then(() => {
+//         console.log("Connected to db");
+//     })
+//     .catch((err) => {
+//         console.log(err.message);
+//     });
+
+//     module.exports = mongoose;
