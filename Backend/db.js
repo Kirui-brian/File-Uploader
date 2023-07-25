2ef65e06cfb2da15794da156fb8e3f10cb2841c0
Kirui-brian <brinkiru@gmail.com>
2023-07-25 09:48:31 +0300

    update database connection

--- Backend/db.js
+++ Backend/db.js
@@ -1,17 +1,33 @@
+// const mongoose = require('mongoose');
+
+// //MongoDB connection URL
+// const dbURL = 'mongodb://127.0.0.1:27017/file-uploader';
+
+// mongoose.connect(dbURL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//     console.log('Connected to MongoDB!');
+// });
+
+// module.exports = mongoose;
+
 const mongoose = require('mongoose');
+import dotenv from "dotenv"; 
 
-//MongoDB connection URL
-const dbURL = 'mongodb://127.0.0.1:27017/file-uploader';
+dotenv.config();
 
-mongoose.connect(dbURL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Connected to db");
+    })
+    .catch((err) => {
+        console.log(err.message);
+    });
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-    console.log('Connected to MongoDB!');
-});
-
-module.exports = mongoose;
+    module.exports = mongoose;
