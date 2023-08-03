848532a2590baaf023eee615918663a3c30f120b
Kirui-brian <brinkiru@gmail.com>
2023-08-03 17:38:18 +0300

    update routes file

--- Frontend/src/router/routes.js
+++ Frontend/src/router/routes.js
@@ -4,8 +4,37 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/FormPage.vue') },
-      { path: 'form-data-list', component: () => import('pages/FormDataList.vue'), name: 'FormDataList' }
+      {
+        path: '',
+        component: () => import('pages/LoginSignUp.vue'),
+        name: 'LoginSignUp',
+        meta: { requiresAuth: false }, // Set requiresAuth to false as this page is accessible without authentication
+      },
+      { path: 'form-page',
+      component: () => import('pages/FormPage.vue'),
+      meta: { requiresAuth: true }, // Set requiresAuth to true as this page requires authentication
+      beforeEnter: (to, from, next) => {
+        const authStore = useAuthStore();
+        if (authStore.isLoggedIn) {
+          next();
+        } else {
+          next('/');
+        }
+      },
+    },
+      { path: 'form-data-list',
+      component: () => import('pages/FormDataList.vue'),
+      name: 'FormDataList',
+      meta: { requiresAuth: true},
+      beforeEnter: (to, from, next) => {
+        const authStore = useAuthStore();
+        if (authStore.isLoggedIn) {
+          next();
+        } else {
+          next('/');
+        }
+      },
+    }
     ]
   },
 
