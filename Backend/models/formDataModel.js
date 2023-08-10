ad485e3cf177579d778aa5a71541ba26c42ea2d8
Kirui-brian <brinkiru@gmail.com>
2023-08-10 20:32:36 +0300

    update on models

--- Backend/models/formDataModel.js
+++ Backend/models/formDataModel.js
@@ -2,7 +2,7 @@ const mongoose = require('../db');
 
 const formDataSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, lowercase: true },
     phoneNumber: { type: String, required: true },
     filePath: { type: String, required: true },
 });
